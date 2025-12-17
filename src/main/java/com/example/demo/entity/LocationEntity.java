b69d4e6aab9afaa51b1036ed8d5d4ebde59b8eee
bala-amypo <bala@amypo.in>
2025-12-17 05:23:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/LocationEntity.java
+++ src/main/java/com/example/demo/entity/LocationEntity.java
@@ -19,6 +19,40 @@ public class LocationEntity {
     }
 
     public void setId(Long id) {
-        this.id = id
+        this.id = id;
     }
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+    return name;
+    }
+    public void setName(String name) {
+    this.name = name;
+}
+public Double getLatitude() {
+    return latitude;
+}
+public void setLatitude(Double latitude) {
+    this.latitude = latitude;
+}
+public Double getLongitude() {
+    return longitude;
+}
+public void setLongitude(Double longitude) {
+    this.longitude = longitude;
+}
+public LocationEntity() {
+}
+public LocationEntity(long id, String name, Double latitude, Double longitude) {
+    this.id = id;
+    this.name = name;
+    this.latitude = latitude;
+    this.longitude = longitude;
+}
+
 }
